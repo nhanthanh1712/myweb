117faee91aaab201005d7748e4ba18fdc4969c33
nhanthanh1712 <104672138+nhanthanh1712@users.noreply.github.com>
2023-10-26 17:06:05 +0700

    first commit

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms import DateTimeLocalField, StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo, InputRequired
 
 
@@ -32,6 +32,27 @@ class TaskForm(FlaskForm):
     inputDescription=StringField('Task Description',
         [DataRequired(message="Please enter your task content!")])
     inputPriority=SelectField('Priority', coerce=int)
-
+    inputProjectName = SelectField('Project', coerce=int)
+    inputStatus = SelectField('Status', coerce=int)
+    inputStatusRO = StringField('inputStatus')
+    inputDeadline = DateTimeLocalField('Project Deadline', format='%Y-%m-%dT%H:%M', 
+        validators=[DataRequired(message="Please select a valid deadline")])
     submit1=SubmitField('Create Task')
     submit2=SubmitField('Edit Task')
+    def set_status_choices(self, statuses):
+        self.inputStatus.choices = [(status.status_id, status.description) for status in statuses]
+
+class ProjectForm(FlaskForm):
+    inputName=StringField('Project Name',
+        [DataRequired(message="Please enter your project name!")])
+    inputDescription=StringField('Project Description',
+        [DataRequired(message="Please enter your project description!")])
+    inputDeadline = DateTimeLocalField('Project Deadline', format='%Y-%m-%dT%H:%M', 
+        validators=[DataRequired(message="Please select a valid deadline")])
+    inputStatus=SelectField('Status', coerce=int)
+    
+    submit=SubmitField('Create Project')
+    
+class SearchForm(FlaskForm):
+    search_query = StringField('Search', validators=[DataRequired()])
+    submit = SubmitField('Search')
--- tpl.py
+++ tpl.py
@@ -1,12 +1,12 @@
 from flask import Flask, redirect, render_template, request, flash, session
-from forms import SignUpForm, SignInForm, TaskForm
+from forms import SignUpForm, SignInForm, TaskForm, ProjectForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 basedir=os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='Khang Python-Flask Web App'
+app.config['SECRET_KEY']='Nhan Python-Flask Web App'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'app.db')
 
 app.config['SQLITECHEMY_TRACK_MODIFICATIONS']=False
@@ -95,27 +95,48 @@ def logOut():
     session.pop('user', None)
     return redirect('/userHome')
 
-@app.route('/newTask',methods=['GET','POST'])
+# @app.route('/newTask',methods=['GET','POST'])
+# def newTask():
+#     a_user_id=session.get('user')
+#     form=TaskForm()
+#     form.inputProjectName.choices=[(p.project_id, p.name) for p in db.session.query(models.Project).all()]
+#     form.inputPriority.choices=[(p.priority_id,p.text) for p in db.session.query(models.Priority).all()]
+#     form.inputStatus.choices=[(p.status_id,p.description) for p in db.session.query(models.Status).all()]
+@app.route('/newTask', methods=['GET', 'POST'])
 def newTask():
-    a_user_id=session.get('user')
-    form=TaskForm()
+    a_user_id = session.get('user')
+    form = TaskForm()
+    form.inputProjectName.choices=[(p.project_id, p.name) for p in db.session.query(models.Project).all()]
     form.inputPriority.choices=[(p.priority_id,p.text) for p in db.session.query(models.Priority).all()]
+    form.inputStatus.choices=[(p.status_id,p.description) for p in db.session.query(models.Status).all()]
+    statuses = db.session.query(models.Status).all()
+    form.set_status_choices(statuses)
+
+
     if a_user_id:
         user=db.session.query(models.User).filter_by(user_id=a_user_id).first()
         
         if form.validate_on_submit():
             _description=form.inputDescription.data
             _priority_id=form.inputPriority.data
+            _deadline=form.inputDeadline.data
+            _project_id=form.inputProjectName.data
+            _status_id=form.inputStatus.data
             priority=db.session.query(models.Priority).filter_by(priority_id=_priority_id).first()
-
+            project=db.session.query(models.Project).filter_by(project_id=_project_id).first()
+            status=db.session.query(models.Project).filter_by(status_id=_status_id).first()
             _task_id=request.form['hiddenTaskId']
+            if project and _deadline > project.deadline:
+                flash("Deadline of the task cannot be later than the project's deadline.", "error")
+                return render_template('/newTask.html', form=form, user=user)
             if (_task_id=="0"):
-                task=models.Task(description=_description, user=user, priority=priority)
+                task=models.Task(description=_description, priority=priority, deadline=_deadline, project=project,status=status)
                 db.session.add(task)
             else:
                 task = db.session.query(models.Task).filter_by(task_id=_task_id).first()
                 task.description=_description
                 task.priority=priority
+                task.deadline=_deadline
 
             db.session.commit()
             return redirect('/userHome')
@@ -136,31 +157,145 @@ def deleteTask():
 
 @app.route('/editTask', methods=['GET', 'POST'])
 def editTask():
-    _user_id=session.get('user')
-    form=TaskForm()
-    form.inputPriority.choices=[(p.priority_id, p.text) for p in db.session.query(models.Priority).all()]
+    _user_id = session.get('user')
+    form = TaskForm()
+    form.inputPriority.choices = [(p.priority_id, p.text) for p in db.session.query(models.Priority).all()]
+
     if _user_id:
-        user=db.session.query(models.User).filter_by(user_id=_user_id).first()
-        _task_id=request.form['hiddenTaskId']
+        _task_id = request.form['hiddenTaskId']
         if _task_id:
-            task=db.session.query(models.Task).filter_by(task_id=_task_id).first()
-            form.inputDescription.default=task.description
-            form.inputPriority.default=task.priority_id
-            form.process()
-            return render_template('/newtask.html', form=form, user=user, task=task)
+            task = db.session.query(models.Task).filter_by(task_id=_task_id).first()
+            if task:
+                if task.completed:
+                    # If the task is already completed, set it as not completed
+                    task.completed = False
+                else:
+                    # If the task is not completed, set it as completed
+                    task.completed = True
+
+                db.session.commit()
+            if form.validate_on_submit():
+                _status_id = form.inputStatus.data
+                status = db.session.query(models.Status).filter_by(status_id=_status_id).first()
+                
+                if status:
+                    task.status = status
+
+                    # Check if all tasks in the project are completed
+                    project = task.project
+                    if all(task.status_id == 3 for task in project.tasks):
+                        project.status_id = 3  # Set project status to "Completed"
+                    elif any(task.status_id == 1 for task in project.tasks):
+                        project.status_id = 1  # Set project status to "In Progress"
+                    else:
+                        project.status_id = 2  # Set project status to "Not Started"
+
+                    db.session.commit()
+                    return redirect('/userHome')
+                else:
+                    flash('Invalid status selected.')
+                    return redirect('/editTask', task=task, form=form)
+
+            return render_template('/newTask.html', form=form, task=task)
+        else:
+            flash('Invalid task selected.')
+            return redirect('/userHome')
+
     return redirect('/')
 
-@app.route('/doneTask', methods=['GET', 'POST'])
+@app.route('/doneTask', methods=['POST'])
 def doneTask():
+    _user_id = session.get('user')
+    _task_id = request.form.get('hiddenTaskId')
+    if _user_id and _task_id:
+        task = db.session.query(models.Task).filter_by(task_id=_task_id).first()
+        if task:
+            task.status_id = 3  # Set task status to "Done"
+
+            # Check if all tasks in the project are completed
+            project = task.project
+            if all(t.status_id == 3 for t in project.tasks):
+                project.status_id = 3  # Set project status to "Completed"
+            elif any(t.status_id == 1 for t in project.tasks):
+                project.status_id = 1  # Set project status to "In Progress"
+            else:
+                project.status_id = 2  # Set project status to "Not Started"
+
+            db.session.commit()
+
+    return redirect('/userHome')
+
+
+@app.route('/newProject',methods=['GET','POST'])
+def newProject():
+    a_user_id=session.get('user')
+    form=ProjectForm()
+    form.inputStatus.choices=[(s.status_id,s.description) for s in db.session.query(models.Status).all()]
+    if a_user_id:
+        user=db.session.query(models.User).filter_by(user_id=a_user_id).first()
+        
+        if form.validate_on_submit():
+            _name=form.inputName.data
+            _description=form.inputDescription.data
+            _deadline=form.inputDeadline.data
+            _status_id=form.inputStatus.data
+            status=db.session.query(models.Status).filter_by(status_id=_status_id).first()
+
+            _project_id=request.form['hiddenProjectId']
+            if (_project_id=="0"):
+                project=models.Project(name=_name, description=_description,deadline=_deadline, user=user, status=status)
+                db.session.add(project)
+            else:
+                project = db.session.query(models.Project).filter_by(project_id=_project_id).first()
+                project.name=_name
+                project.description=_description
+                project.status=status
+
+            db.session.commit()
+            return redirect('/userHome')
+        return render_template('/newProject.html', form=form, user=user)
+    return redirect('/')
+
+@app.route('/deleteProject', methods=['GET','POST'])
+def deleteProject():
     _user_id=session.get('user')
     if _user_id:
-        _task_id=request.form['hiddenTaskId']
-        if _task_id:
-            task=db.session.query(models.Task).filter_by(task_id=_task_id).first()
-            task.isCompleted=True
+        _project_id=request.form['hiddenProjectId']
+        if _project_id:
+            project=db.session.query(models.Project).filter_by(project_id=_project_id).first()
+            db.session.delete(project)
             db.session.commit()
         return redirect('/userHome')
     return redirect('/')
+
+@app.route('/editProject', methods=['GET', 'POST'])
+def editProject():
+    _user_id=session.get('user')
+    form=ProjectForm()
+    form.inputStatus.choices=[(p.status_id, p.description) for p in db.session.query(models.Status).all()]
+    if _user_id:
+        _project_id=request.form['hiddenProjectId']
+        if _project_id:
+            project=db.session.query(models.Project).filter_by(project_id=_project_id).first()
+            form.inputName.default=project.name
+            form.inputDescription.default=project.description
+            form.inputDeadline.default=project.deadline
+            form.inputStatus.default=project.status_id
+            form.process()
+            return render_template('/newProject.html', form=form, project=project, user=_user_id)
+    return redirect('/')
+@app.route('/search', methods=['POST'])
+def search():
+    status = request.form.get('status')
+    if status:
+        status = int(status)
+        projects = db.session.query(models.Project).filter(models.Project.status_id == status).all()
+        tasks = db.session.query(models.Task).filter(models.Task.status_id == status).all()
+    else:
+        projects = db.session.query(models.Project).all()
+        tasks = db.session.query(models.Task).all()
+    return render_template('search_results.html', projects=projects, tasks=tasks)
+
 if __name__=='__main__':
     app.run(host='127.0.0.1', port='8080', debug=True)
 
